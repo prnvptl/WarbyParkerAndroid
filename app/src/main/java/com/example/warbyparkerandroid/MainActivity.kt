e894a463a69f0aa091b937510335033dde7a1ff5
Pranav-794 <pranavvpatell@gmail.com>
2022-05-02 14:36:46 -0400

    Initial commit

---
+++ app/src/main/java/com/example/warbyparkerandroid/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.warbyparkerandroid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.warbyparkerandroid.ui.theme.WarbyParkerAndroidTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SystemUIControl()
+            WarbyParkerAndroidTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+
+        }
+    }
+}
+
+@Composable
+fun SystemUIControl() {
+    // Remember a SystemUiController
+    val systemUiController = rememberSystemUiController()
+    val useDarkIcons = MaterialTheme.colors.isLight
+    val backgroundColor = MaterialTheme.colors.background
+    SideEffect {
+        // Update all of the system bar colors to be transparent, and use
+        // dark icons if we're in light theme
+        systemUiController.setSystemBarsColor(
+            color = backgroundColor,
+            darkIcons = useDarkIcons
+        )
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    WarbyParkerAndroidTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/warbyparkerandroid/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.example.warbyparkerandroid.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Blue200 = Color(0xFF449dd8)
+val Blue700 = Color(0xFF7dceff)
+val Blue800 = Color(0xFF006fa6)
+val White100 = Color(0xFFFFFFFF)
